8f0771bef8d87ec64b26bfbc0ee8851fc01ce820
mikael <mikael.programing.c@gmail.com>
2024-06-04 18:26:17 +0100

    pls

--- h-1/Program.cs
+++ h-1/Program.cs
@@ -1,8 +1,27 @@
-﻿var name = "Ewa";
-char sex = 'F';
-int age = 30;
+﻿var name1 = "Ewa";
+char sex1 = 'F';
+int age1 = 30;
 
-if (name == "Ewa" && sex == 'F' && age == 30)
+if (name1 == "Ewa" && sex1 == 'F' && age1 == 30)
 {
     Console.WriteLine("Ewa, 30 lat");
+
+}
+
+var name2 = "Brak";
+char sex2 = 'F';
+int age2 = 26;
+
+if (name2 == "Brak" && sex2 == 'F' && age2 < 30)
+{
+    Console.WriteLine("kobieta ponisay 30 lat");
+}
+
+var name3 = "Brak";
+char sex3 = 'M';
+int age3 = 14; 
+
+if (name3 == "Brak" && sex3 == 'M' && age3 < 18)
+{
+    Console.WriteLine("Neponoletni Menscisna");
 }
